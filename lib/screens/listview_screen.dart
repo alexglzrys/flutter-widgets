86bb591c8ed37a64fa49a5a020f540cc034937ba
Alejandro González Reyes <alexglzrys@gmail.com>
2022-02-07 11:42:42 -0600

    Taps en ListTiles y estilos del AppBar

--- lib/screens/listview_screen.dart
+++ lib/screens/listview_screen.dart
@@ -10,6 +10,10 @@ class ListviewScreen extends StatelessWidget {
     return Scaffold(
         appBar: AppBar(
           title: const Text('Listview Personalizado'),
+          // Retirar sombra en la barra de titulo
+          elevation: 0,
+          // Cambiar color de fondo
+          backgroundColor: Colors.deepOrange,
         ),
         // Listview tiene varios constructores por nombre para mostrar de diferente manera el listado de sus elementos
         body: ListView.separated(
@@ -17,6 +21,12 @@ class ListviewScreen extends StatelessWidget {
             itemBuilder: (context, index) => ListTile(
                   title: Text(opcionesMenu[index]),
                   trailing: Icon(Icons.arrow_forward_ios_outlined),
+                  // Lost ListTitle tienen una propiedad para interactuar cuando son presionados
+                  // Los onTap se pueden anular con un valor null
+                  // Algunas veces necesitaremos un ternario para activar la acción o dejarlo en null según la data de nuestra aplicación
+                  onTap: () {
+                    print(opcionesMenu[index]);
+                  },
                 ),
             // Widget a renderizar como separador de cada elemento en la lista
             // Un estandar en Dart es que si no se usan los argumentos se renombren con guiones bajos (Esto es vàlido para nombrar varibales)
