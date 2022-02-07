144ec2b580af267831465d0ed888a6621b3c3d0d
Alejandro González Reyes <alexglzrys@gmail.com>
2022-02-07 12:04:30 -0600

    HomeScreen y Rutas iniciales

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,8 @@
-import 'package:componentes_app/screens/listview_screen.dart';
 import 'package:flutter/material.dart';
+
+// Este paquete exporta todos los componentes a usar como pantallas de nuestra aplicación
+// Evita el tener que hacer muchos imports en esta sección
+import 'package:componentes_app/screens/screens.dart';
 
 void main() => runApp(const MyApp());
 
@@ -9,8 +12,18 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-        debugShowCheckedModeBanner: false,
-        title: 'Material App',
-        home: ListviewScreen());
+      debugShowCheckedModeBanner: false,
+      title: 'Material App',
+      // home: ListviewScreen()
+
+      // Cuando se trabaja con rutas no hay un home inicial, existe una ruta inicial
+      initialRoute: 'home',
+      // El listado de rutas requiere una ruta y un componente a mostrar para esa ruta
+      routes: {
+        'home': (BuildContext context) => HomeScreen(),
+        'listview': (BuildContext context) => ListviewScreen(),
+        'listview_standar': (BuildContext context) => ListviewStandarScreen()
+      },
+    );
   }
 }
---
+++ lib/screens/screens.dart
@@ -0,0 +1,13 @@
+/// Archivo de exportaciones de componentes de pantalla
+///
+/// Este patrón ayuda a no saturar con muchas importaciones nuestro punto de entrada
+/// de nuestra App.
+///
+/// Esto se da generalmente cuando nuestra App cuenta con muchas vistas en su
+/// sistema de router
+///
+/// Generalmente el nombre de este archivo es el mismo que el de su carpeta contenedora
+
+export 'package:componentes_app/screens/home_screen.dart';
+export 'package:componentes_app/screens/listview_screen.dart';
+export 'package:componentes_app/screens/listview_standar_screen.dart';
